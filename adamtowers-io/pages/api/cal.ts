4262be1745f16975901d7f77bbdbe7477a08d347
Adam Towers <adam.towers77@gmail.com>
2021-10-01 19:34:12 -0700

    revalidation for cal

--- adamtowers-io/pages/api/cal.ts
+++ adamtowers-io/pages/api/cal.ts
@@ -2,4 +2,8 @@ import { getEvents } from "data/Cal";
 
 export default async function handler(req, res) {
     res.send({ events: await getEvents() })
+
+    return {
+        revalidate: 2 * 60
+    }
 }
